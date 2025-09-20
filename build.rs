60e5af2eca5707a53d76b9381c9a7d128605d0f6
Egor-Urban <egor.zhurban@gmail.com>
2025-09-20 10:09:52 +0300

    Upd test layout

--- build.rs
+++ build.rs
@@ -1,7 +1,7 @@
 fn main() {
     let config =
     slint_build::CompilerConfiguration::new()
-    .with_style("cupertino-dark".into());
+    .with_style("material-dark".into());
 
     slint_build::compile_with_config("ui/app-window.slint", config).unwrap();
 }
